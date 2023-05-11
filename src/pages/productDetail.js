27bb275b86b2b555376d44709bac3886c5b539f5
AiutoPcAmico <andrea@andreafelappi.it>
2023-05-11 13:49:59 +0200

    Added quantity buttons
    Co-authored-by: DonaKing-0 <DonaKing-0@users.noreply.github.com>

--- src/pages/productDetail.js
+++ src/pages/productDetail.js
@@ -6,6 +6,8 @@ import baseImages from "../img/base_image_temp.json";
 function ProductDetail() {
   const params = useParams();
   const { darkMode } = useContext(DarkModeContext);
+  const [height, setHeight] = useState(0);
+  const [quantity, setQuantity] = useState(1);
   const [product, setProduct] = useState({
     id: null,
     name: "",
@@ -13,9 +15,49 @@ function ProductDetail() {
     description: "",
     unitPrice: null,
     isActive: true,
+    quantity: 1,
     foto: "",
+    //pezzi magazzino
   });
   const idOfProduct = parseInt(params.id);
+
+  useEffect(() => {
+    setHeight(document.getElementById("testo").clientHeight);
+  }, []);
+
+  //gestisce l'on blur nel caso di testo vuoto!
+  const cambiaquantita = (event) => {
+    console.log(event.target.value);
+    event.target.value = event.target.value * 1;
+
+    if (isNaN(event.target.value)) {
+      event.target.value = 1;
+      console.log("NON numero");
+    }
+    if (event.target.value < 1) {
+      event.target.value = 1;
+    }
+    event.target.value = +event.target.value;
+
+    //setQuantity(event.target.value * 1);
+    changeQuantity(event.target.value, true);
+  };
+
+  //gestisce tutti gli altri casi!
+  function changeQuantity(many, manual) {
+    //if (many === "");
+
+    if (many !== "") {
+      var pippo = many * 1;
+
+      if (pippo < 1) setQuantity(1);
+      if (pippo > quantity) setQuantity(product.quantity);
+
+      if (pippo >= 1 && pippo <= product.quantity) setQuantity(pippo);
+    } else {
+      setQuantity("");
+    }
+  }
 
   async function retrieveDetailsOfProduct(idProd) {
     var array = [];
@@ -29,6 +71,7 @@ function ProductDetail() {
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu ultrices elit. Aenean interdum, tortor non maximus efficitur, urna leo ultricies ante, at viverra nunc lorem a lacus. Nam ac pulvinar diam. Nulla tempor erat dictum, ultrices magna eget, sodales nibh. Aenean eu hendrerit mauris, sed suscipit justo. Integer euismod laoreet lorem sed rutrum. In hac habitasse platea dictumst. Fusce nec diam eleifend, ultrices massa et, laoreet urna. Praesent fringilla, ex nec varius tincidunt, enim magna dapibus orci, fermentum scelerisque urna tellus non nulla. Aliquam interdum finibus mollis.\nFusce pretium nulla vel viverra accumsan. Mauris a mi pharetra, consequat magna ac, laoreet mauris. Mauris at consectetur nunc. Proin sit amet ligula suscipit, lacinia mauris at, euismod mi. Donec vestibulum mi non consectetur tempor. Aenean mattis lacus vel lorem posuere placerat. Maecenas quis ligula quis lacus convallis pellentesque non eget leo. Ut maximus eros diam, a venenatis ligula congue nec. Nulla risus ante, viverra in massa ac, congue fermentum est. Sed quis commodo risus, ac molestie augue. Phasellus auctor justo ipsum, ut volutpat enim condimentum sed. Ut finibus enim a sapien porttitor, nec lacinia nisi ornare. In non risus libero. Maecenas mollis congue mauris eu ultricies.\nSed quis sagittis odio. Nunc ullamcorper nulla nunc, vel imperdiet lorem tempus eget. Quisque mattis imperdiet commodo. Cras aliquam dolor non purus sodales faucibus. Curabitur eget dapibus enim. Praesent lacinia libero turpis, quis tincidunt risus viverra id. Pellentesque sapien orci, efficitur vel turpis quis, porttitor pretium lacus. Suspendisse feugiat turpis nec purus faucibus efficitur. Nullam vel sodales erat. Morbi molestie, elit a maximus congue, libero augue vestibulum erat, id condimentum odio enim et nibh. Vestibulum lacinia odio odio, quis ultrices lacus luctus posuere. Morbi luctus ante a mollis varius. In tristique lacinia facilisis. Aliquam venenatis tortor vel maximus imperdiet. Mauris sem magna, sollicitudin ut finibus sit amet, aliquet ac ipsum.\nSuspendisse et sem velit. Maecenas quam libero, cursus et efficitur iaculis, consectetur sit amet leo. Ut fermentum volutpat lobortis. Mauris non neque eget nisl posuere commodo id in ligula. Nunc sed fermentum quam. Cras aliquam porta laoreet. Cras eget eleifend purus. In malesuada laoreet purus et mollis. Nullam sagittis consectetur pharetra. Cras eu euismod massa. Integer auctor velit massa, vitae bibendum quam pharetra sed. Donec nec massa tortor. Pellentesque euismod libero ac est mollis, et commodo sapien imperdiet. Praesent ac convallis elit, ut imperdiet purus. Donec felis enim, lacinia vel porta dictum, convallis ut velit. Maecenas eu varius orci, in tempor ipsum.\nMorbi eget quam et felis ullamcorper cursus. Vivamus ipsum nibh, viverra et est ut, placerat dignissim quam. Vestibulum varius justo vitae fermentum rhoncus. Aliquam tincidunt nibh leo, accumsan fringilla nibh condimentum ac. Aliquam ac pulvinar libero. Cras purus dui, finibus nec egestas in, malesuada vel nibh. Curabitur facilisis lacus pellentesque eros dictum eleifend. Cras molestie ornare lectus quis feugiat. Nunc convallis euismod felis iaculis pharetra. Etiam a ligula condimentum ipsum tristique sollicitudin. Phasellus egestas nulla nec quam sagittis dictum. Aliquam erat volutpat. Pellentesque suscipit ligula dapibus turpis iaculis, porttitor vehicula lectus fringilla. Aenean dignissim, arcu vitae convallis ultrices, sapien est mollis diam, vitae facilisis eros felis sed mi.",
         unitPrice: 10,
         isActive: true,
+        quantity: 23,
         foto: baseImages.fazzoletti,
       },
       {
@@ -38,6 +81,7 @@ function ProductDetail() {
         description: "Pacchetto da 200",
         unitPrice: 30,
         isActive: true,
+        quantity: 10,
         foto: baseImages.fazzoletti,
       },
       {
@@ -47,6 +91,7 @@ function ProductDetail() {
         description: "Una bellissima torre da ricarica",
         unitPrice: 89.2,
         isActive: true,
+        quantity: 60,
         foto: baseImages.ricaricatore,
       },
       {
@@ -57,6 +102,7 @@ function ProductDetail() {
           "Una bellissima scatoletta per contenere i tuoi fazzoletti!",
         unitPrice: 21.2,
         isActive: true,
+        quantity: 1,
         foto: baseImages.scatolina,
       },
     ];
@@ -80,6 +126,7 @@ function ProductDetail() {
       <div className="detailsPage">
         <h2 className={darkMode ? "testolight" : "testodark"}>
           {product.name}
+          {height}
         </h2>
         <div className=" text flex-column" style={{}}>
           <div className="row flex-wrap align-items-center pb-3">
@@ -105,10 +152,11 @@ function ProductDetail() {
               style={{ width: "32%", overflowY: "scroll", maxHeight: "60vh" }}
             >
               <div>
-                {/*<p style={{ fontSize: "25px", textAlign: "right" }}>
+                <p style={{ fontSize: "25px", textAlign: "right" }}>
+                  prezzo unitario{" "}
                   {(Math.round(product.unitPrice * 100) / 100).toFixed(2)} €
-            </p>*/}
-                <p>{product.description}</p>
+                </p>
+                <p id="testo">{product.description}</p>
               </div>
             </div>
             <div
@@ -121,6 +169,50 @@ function ProductDetail() {
               <p style={{ fontSize: "25px", textAlign: "right" }}>
                 {(Math.round(product.unitPrice * 100) / 100).toFixed(2)} €
               </p>
+
+              <p>
+                <div className="input-group mb-3 justify-content-center">
+                  <div className="input-group-prepend">
+                    <button
+                      className="btn btn-danger"
+                      type="button"
+                      id="button-addon1"
+                      onClick={() => {
+                        changeQuantity(quantity - 1, false);
+                      }}
+                    >
+                      <i className="bi bi-dash"></i>
+                    </button>
+                  </div>
+                  <input
+                    type="number"
+                    className="form-control text-center"
+                    placeholder="Quantity..."
+                    aria-describedby="button-addon1"
+                    value={quantity}
+                    onChange={(event) => {
+                      changeQuantity(event.target.value, true);
+                    }}
+                    onBlur={(event) => {
+                      cambiaquantita(event);
+                    }}
+                    style={{ width: "100px", flex: "none" }}
+                  />
+                  <div class="input-group-append">
+                    <button
+                      class="btn btn-info"
+                      type="button"
+                      id="button-addon2"
+                      onClick={() => {
+                        changeQuantity(quantity + 1, false);
+                      }}
+                    >
+                      <i class="bi bi-plus"></i>
+                    </button>
+                  </div>
+                </div>
+              </p>
+
               <p>
                 <button
                   type="button"
@@ -132,12 +224,18 @@ function ProductDetail() {
                   aggiungi al carrello
                 </button>
               </p>
-              <p>
-                Per ora la spedizione è gratuita!{}
-                {/*{
-                     height 
-                  }*/}
-              </p>
+
+              <div style={{ textAlign: "left" }}>
+                <p>
+                  <b>Quantità in deposito: </b> {product.quantity}
+                </p>
+                <p>
+                  <b>Spedizione:</b> Ancora per poco gratuita!
+                </p>
+                <p>
+                  <b>Categoria:</b> {product.category}
+                </p>
+              </div>
             </div>
           </div>
         </div>
