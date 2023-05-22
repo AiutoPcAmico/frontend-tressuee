8f9010db7dc8d4d74b255c17d692ffcce83a1ff1
abdelazizomar133@gmail.com <Abdelazizomar1>
2023-05-22 14:30:07 +0200

    contacts

--- src/pages/ContactForm.js
+++ src/pages/ContactForm.js
@@ -1,61 +1,45 @@
-/*
 import React from "react";
-const ContactForm = () => {
-  const [formStatus, setFormStatus] = React.useState("Send");
+import "../App.css";
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    setFormStatus("Submitting...");
-    const { name, email, message } = e.target.elements;
-    let conFom = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    console.log(conFom);
+const ContactForm = () => {
+  const handleClick = (url) => {
+    window.open(url, "_blank");
   };
+
   return (
-    <div className="container mt-5">
-      <h2 className="mb-3">Contact Form </h2>
-      <form onSubmit={onSubmit}>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="name">
-            Name
-          </label>
-          <input className="form-control" type="text" id="name" required />
-        </div>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="email">
-            Email
-          </label>
-          <input className="form-control" type="email" id="email" required />
-        </div>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="message">
-            Message
-          </label>
-          <textarea className="form-control" id="message" required />
-        </div>
-        <button className="btn btn-danger" type="submit">
-          {formStatus}
-        </button>
-      </form>
+    <div className="social-icons">
+      <div
+        className="social-icon"
+        onClick={() => handleClick("https://www.instagram.com")}
+      >
+        <img src="../img/instagram-icon.png" alt="Instagram" />
+      </div>
+      <div
+        className="social-icon"
+        onClick={() => handleClick("https://www.facebook.com")}
+      >
+        <img src="../img/facebook-icon.png" alt="Facebook" />
+      </div>
+      <div
+        className="social-icon"
+        onClick={() => handleClick("https://www.linkedin.com")}
+      >
+        <img src="../img/linkedin-icon.png" alt="LinkedIn" />
+      </div>
+      <div
+        className="social-icon"
+        onClick={() => handleClick("https://www.gmail.com")}
+      >
+        <img src="../img/gmail-icon.png" alt="Gmail" />
+      </div>
+      <div
+        className="social-icon"
+        onClick={() => handleClick("https://www.twitter.com")}
+      >
+        <img src="../img/twitter-icon.png" alt="Twitter" />
+      </div>
     </div>
   );
 };
-export { ContactForm }; */
-
-function ContactForm() {
-  return (
-    <div>
-      <img src={require("../img/gmail_new_logo_icon_159149.png")} alt="logo" />
-
-      <img
-        src={require("../img/linkedin-logo-linkedin-icon-transparent-free-png.png")}
-        alt="logo"
-      />
-    </div>
-  );
-}
 
 export { ContactForm };
